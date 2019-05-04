196f50e3b98f938258342650131320ed593c7899
Casey Price <Casey.Price@coxautoinc.com>
2019-05-03 20:15:55 -0400

    rewrite submit with async await

--- server/routes/submissions.js
+++ server/routes/submissions.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Submission = require("../models/submission")
 
+
+var saveSubmission = (newSubmission, res) => {
+  newSubmission.save().then(sub => {
+      res.status(200).json({
+        msg: "Submission successfully entered.",
+        data: sub
+      });
+  }).catch(err => {
+    res.status(500).json({ msg: `${err}`})
+  });
+
+}
+
 //@@ GET /api/submissions/
 //@@ Display all submissions
 router.get('/', function(req, res){
@@ -28,9 +41,12 @@ router.get('/:contestId', function(req, res){
 })
 
 
+
+
+
 // @@ POST /api/submissions
 // @@ Creates a new level submission
-router.post('/', function(req, res){
+router.post('/', async function(req, res){
   const newSubmission = new Submission({
     contestId: req.body.contestId,
     dateSubmitted: req.body.dateSubmitted,
@@ -41,62 +57,32 @@ router.post('/', function(req, res){
     overwrite: req.body.overwrite
   });
 
-  if(req.body.overwrite == true){
-    newSubmission.save(function(err, submission){
-      if(err){
-        res.status(500).json({ msg: `${err}`})
-      } else {
+
+  try {
+      //Validate level hasn't already been submitted
+      let existingSubmission = await Submission.find( {$and:[{ lookupCode: req.body.lookupCode }, { contestId: req.body.contestId }]});
+      if(existingSubmission[0] != null) {
         res.status(200).json({
-          msg: "Submission successfully entered.",
-          data: submission
-        })
+          msg: `This level code has already been submitted to this contest.`,
+          submissionId: `${existingSubmission[0]._id} `
+        });
+        return;
       }
-    })  
-  } else {
-    //if lookupCode already in DB, return err
-    //if USER SUBMITTING already exists in the contest, return err, option to overwrite with new submission
-    Submission.find( {$and:[{ lookupCode: req.body.lookupCode }, { contestId: req.body.contestId }]}, function(err, submission){
-      if(err){
-        res.status(500).json({ msg: `${err}` })
-      }else {
-        if(submission[0] == null){
-          //check to see if this user already submitted to this contest
-          Submission.find( {$and:[{ rumpusCreatorId: req.body.rumpusCreatorId}, { contestId: req.body.contestId}]}, function(err, submission){
-            if(err){
-              res.status(500).json({ msg: `${err}` })
-            } else {
-              if(submission[0] == null){
-                // user has not submitted to contest, save to contest 
-                newSubmission.save(function(err, submission){
-                  if(err){
-                    res.status(500).json({ msg: `${err}`})
-                  } else {
-                    // res.send(submission);
-                    res.status(200).json({
-                      msg: "Submission successfully entered.",
-                      data: submission
-                    })
-                  }
-                })        
-              } else {
-                // user has already submitted to contest, can resubmit with a overwrite flag
-                res.status(200).json({ msg: 'User has already submitted to contest with a different level code, can resubmit with a overwrite flag'})
-              }
-            }
-          })
-        } else {
-          if(submission[0].rumpusCreatorId == req.body.rumpusCreatorId){
-            res.status(200).json({ msg: 'User has already submitted to contest with a different level code, can resubmit with a overwrite flag'})
-          } else {
-            res.status(200).json({
-              msg: `This level code has already been submitted to this contest.`,
-              submissionId: `${submission[0]._id} `
-            })
-          }
-        }
+
+      //Validate if user has already submitted a level
+      let existingUserSubmission = Submission.find( {$and:[{ rumpusCreatorId: req.body.rumpusCreatorId}, { contestId: req.body.contestId}]});
+      if(!!existingUserSubmission[0] && !req.body.overwrite) {
+        res.status(200).json({ msg: 'User has already submitted to contest with a different level code, can resubmit with a overwrite flag'});
+        return;
       }
-    })
-  }
+      
+      saveSubmission(newSubmission, res);
+
+    } catch (err) {
+      res.status(500).json({ msg: `${err}` });
+    }
+   
+  
 })
 
 //@@ GET /api/submissions/:submissionId
