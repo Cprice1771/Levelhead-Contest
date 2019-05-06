70d7d0f624bdda0a1edb0da6f8445f2530fdc82b
Evan Vana <evjava@gmail.com>
2019-05-06 15:40:33 -0400

    updated votes route

--- server/routes/votes.js
+++ server/routes/votes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Vote = require("../models/vote")
+const Submission = require("../models/submission")
+
 
 //@@ GET /api/votes
 //@@ Displays all submitted votes
 router.get('/', async (req, res) => {
   try {
     const votes = await Vote.find();
-    if(votes){
-      res.send(votes);
-    }
+    res.send(votes);
   } catch(err){
-    console.log(err);
+    res.status(500).json({ msg: `${err}` });
   }
 })
 
@@ -20,11 +20,10 @@ router.get('/', async (req, res) => {
 router.get('/:contestId', async (req, res) => {
   try {
     const votes = await Vote.find({ contestId: req.params.contestId});
-    if(votes){
-      res.send(votes);
-    }
+    res.send(votes);
+
   } catch(err){
-    console.log(err);
+    res.status(500).json({ msg: `${err}` });
   }
 })
 
@@ -32,20 +31,23 @@ router.get('/:contestId', async (req, res) => {
 //@@ Votes for a new submission
 router.post('/', async (req, res) => {
   try {
-    //check to see if user has voted in this contest already
-    const newVote = new Vote({
-      submissionId: req.body.submissionId,
-      contestId: req.body.contestId,
-      discordId: req.body.discordId,
-      dateVoted: req.body.dateVoted
-    })
-
-    const votes = await newVote.save();
-    // Update submission cache
-    return res.status(200).json({ msg: 'Vote submitted successfully'})
-
+    let alreadyVoted = await Vote.find({$and:[{ discordId: req.body.discordId }, { contestId: req.body.contestId}]})
+    if(alreadyVoted.length > 0){
+      res.status(200).json({ msg: 'You have already voted in this contest.'})
+    } else {
+      let newVote = new Vote({
+        submissionId: req.body.submissionId,
+        contestId: req.body.contestId,
+        discordId: req.body.discordId,
+        dateVoted: req.body.dateVoted
+      })
+  
+      let vote = await newVote.save();
+      let update  = await Submission.findOneAndUpdate( {_id: req.body.submissionId}, {$push: {votes: vote._id}})
+      res.status(200).json({ msg: 'Vote submitted successfully'})
+    }
   } catch (err){
-    res.send(err);
+    res.status(500).json({ msg: `${err}` });
   }
 })
 module.exports = router;
