cdc6ae88c8797977dd34b41ab25aebfecd222248
Casey Price <Casey.Price@coxautoinc.com>
2019-05-12 00:09:49 -0400

    git rid of this cors thing?

--- server/index.js
+++ server/index.js
@@ -21,19 +21,19 @@ let votes = require('./routes/votes')
 app.use('/api/votes', votes)
 
 // Set up a whitelist and check against it:
-var whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://levelcup.herokuapp.com/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// var whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://levelcup.herokuapp.com/']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-// Then pass them to cors:
-app.use(cors(corsOptions));
+// // Then pass them to cors:
+// app.use(cors(corsOptions));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
